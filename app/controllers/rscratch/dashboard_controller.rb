49486ba988e84e285a5be3da9a6f7deac8aa0e14
Avishek Jana <avishekjana.it@gmail.com>
2016-01-26 22:40:35 +0530

    dashboard chart data generated

--- app/controllers/rscratch/dashboard_controller.rb
+++ app/controllers/rscratch/dashboard_controller.rb
@@ -3,9 +3,10 @@ require_dependency "rscratch/application_controller"
 module Rscratch
   class DashboardController < ApplicationController
     def index
-      @exceptions = Rscratch::Exception.all
+      @activity_log = Rscratch::ExceptionLog.select("count(id) as exception_count, date(created_at) as date").group("date(created_at)").order("date(created_at)").last(30)
       respond_to do |format|
         format.html # index.html.erb
+        format.json { render json: @activity_log }
       end      
     end
   end
