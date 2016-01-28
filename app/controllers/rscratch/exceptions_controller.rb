3deb00fc571915823e22ee8c8794506837efe913
Avishek Jana <avishekjana.it@gmail.com>
2016-01-28 08:26:34 +0530

    Smart listing implemented

--- app/controllers/rscratch/exceptions_controller.rb
+++ app/controllers/rscratch/exceptions_controller.rb
@@ -1,8 +1,16 @@
 require_dependency "rscratch/application_controller"
-
 module Rscratch
   class ExceptionsController < ApplicationController
+    include SmartListing::Helper::ControllerExtensions
+    helper  SmartListing::Helper    
     def index
+      @exceptions = Rscratch::Exception.order("updated_at desc")
+      smart_listing_create :exceptions, @exceptions, partial: "rscratch/exceptions/new_exception_smartlist"
+      respond_to do |format|
+        format.html # index.html.erb
+        format.js
+        format.json { render json: @exceptions }
+      end      
     end
   end
 end
