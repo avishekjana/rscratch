caf60661c9234be139d8d332e409efdb6e146f45
Avishek Jana <avishekjana.it@gmail.com>
2016-01-29 20:03:06 +0530

    Exception details Basic UI implemented

--- app/assets/javascripts/rscratch/exceptions.js
+++ app/assets/javascripts/rscratch/exceptions.js
@@ -1,2 +1,22 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(document).ready(function() {
+  $(".exception-summary").hide();
+  $(document).on("click", ".exception-item", function() {
+    var e_date, e_id, e_message, e_new_count, e_total_count, loader;
+    $(".exception-summary").show();
+    e_id = $(this).data("exception-id");
+    e_message = $(this).data("exception-message");
+    e_date = $(this).data("exception-date");
+    e_total_count = $(this).data("exception-total-count");
+    e_new_count = $(this).data("exception-new-count");
+    $(".exception-item").removeClass("selected");
+    $(".excp-item-" + e_id).addClass("selected");
+    loader = JST['rscratch/templates/loader'];
+    $('.progress-loader').html(loader);
+    $('.exception-message').html(e_message);
+    $('.exception-total-count').html(e_total_count);
+    $('.exception-new-count').html(e_new_count);
+    $('.exception-date').html("Last occured on " + e_date);
+  });
+});
