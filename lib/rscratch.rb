71a1b32ed4a8243b5495b19208439143a8939ccb
Avishek Jana <avishekjana.it@gmail.com>
2016-01-30 17:20:34 +0530

    Config class implemented

--- lib/rscratch.rb
+++ lib/rscratch.rb
@@ -1,4 +1,11 @@
 require "rscratch/engine"
+require "rscratch/configuration"
 
 module Rscratch
+  def self.configure
+    yield configuration
+  end
+  def self.configuration
+    @configuration ||= Configuration.new
+  end  
 end
