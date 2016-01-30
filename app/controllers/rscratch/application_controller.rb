1577e3ac9b33843eaf2341b91c3f10bd583feca3
Avishek Jana <avishekjana.it@gmail.com>
2016-01-30 17:19:16 +0530

    Http basic authentication added

--- app/controllers/rscratch/application_controller.rb
+++ app/controllers/rscratch/application_controller.rb
@@ -1,4 +1,12 @@
 module Rscratch
   class ApplicationController < ActionController::Base
+    before_filter :authenticate
+
+    def authenticate
+      if Rscratch.configuration.authenticate
+        instance_eval(&Rscratch.configuration.authenticate)
+      end
+    end
+    
   end
 end
