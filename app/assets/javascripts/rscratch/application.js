e64efbc335cf93310adc0b2b23b439da6742774b
Avishek Jana <avishekjana.it@gmail.com>
2016-01-31 22:48:43 +0530

    Require tree added to make the gem compatible with Rails 4

--- app/assets/javascripts/rscratch/application.js
+++ app/assets/javascripts/rscratch/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require smart_listing
+//= require_tree .
+
 var dateFormat = function () {
     var    token = /d{1,4}|m{1,4}|yy(?:yy)?|([HhMsTt])\1?|[LloSZ]|"[^"]*"|'[^']*'/g,
         timezone = /\b(?:[PMCEA][SDP]T|(?:Pacific|Mountain|Central|Eastern|Atlantic) (?:Standard|Daylight|Prevailing) Time|(?:GMT|UTC)(?:[-+]\d{4})?)\b/g,
