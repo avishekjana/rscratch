a1231b41f9dc038944a3d102256a6fa04ad009cb
Avishek Jana <avishekjana.it@gmail.com>
2016-02-07 12:52:38 +0530

    Resolve and toggle action added

--- app/controllers/rscratch/exceptions_controller.rb
+++ app/controllers/rscratch/exceptions_controller.rb
@@ -3,6 +3,9 @@ module Rscratch
   class ExceptionsController < ApplicationController
     include SmartListing::Helper::ControllerExtensions
     helper  SmartListing::Helper    
+
+    before_filter :set_exception, :only => [:show, :toggle_ignore, :resolve]
+
     def index
       @exceptions = Rscratch::Exception.order("updated_at desc")
       smart_listing_create :exceptions, @exceptions.by_status('new'), partial: "rscratch/exceptions/exception_smartlist"
@@ -13,11 +16,25 @@ module Rscratch
         format.json { render json: @exceptions }
       end      
     end
+
     def show
-      @excp = Rscratch::Exception.find(params[:id])
       @log = @excp.exception_logs.order("created_at desc").page(params[:page]).per(1)
       @historical_data = @excp.exception_logs.select("count(id) as exception_count, date(created_at) as date").group("date(created_at)").order("date(created_at)").last(30)
       rescue Exception => @error          
+    end  
+
+    def toggle_ignore
+      @excp.toggle_ignore!
+    end
+
+    def resolve
+      @excp.resolve!
     end    
+
+    private
+
+    def set_exception
+      @excp = Rscratch::Exception.find(params[:id])
+    end
   end
 end
