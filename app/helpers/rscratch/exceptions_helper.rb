ce774a00e85437ccff57068036a9966f53926328
Avishek Jana <avishekjana.it@gmail.com>
2016-02-09 23:22:04 +0530

    Issue color code added

--- app/helpers/rscratch/exceptions_helper.rb
+++ app/helpers/rscratch/exceptions_helper.rb
@@ -1,4 +1,13 @@
 module Rscratch
   module ExceptionsHelper
+    def get_issue_color(object)
+      if object.resolved?
+        return "green"
+      elsif object.ignored?
+        return "grey"
+      else
+        return "red"
+      end 
+    end
   end
 end
