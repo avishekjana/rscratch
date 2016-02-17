217cbdea54136f03a2e16d08b8954693cb963eb6
Avishek Jana <avishekjana.it@gmail.com>
2016-02-17 22:42:22 +0530

    Log exception function added directly

--- lib/rscratch.rb
+++ lib/rscratch.rb
@@ -5,7 +5,12 @@ module Rscratch
   def self.configure
     yield configuration
   end
+  
   def self.configuration
     @configuration ||= Configuration.new
   end  
+
+  def self.log_exception(_e,_r)
+    log = Exception.log(_e, _r)
+  end
 end
